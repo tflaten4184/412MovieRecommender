20be624cea23ddf2fae4d57a3f6464d54465760b
jacobdsiefkes <jacobdsiefkes@gmail.com>
2021-11-05 12:29:09 -0500

    Update remove-stop-words.py

--- remove-stop-words.py
+++ remove-stop-words.py
@@ -2,8 +2,18 @@
 #Jacob
 
 import pandas as pd
-from IPython.display import display
+from nltk.corpus import stopwords
 
-## Internal path for accessing data in netflix_titles_nov_2019
-ShowInfo = pd.read_csv(r"netflix_titles_nov_2019.csv")
+pd.read_csv("netflix_titles_nov_2019.csv", encoding="utf-8") 
 
+file = pd.read_csv("netflix_titles_nov_2019.csv") 
+
+file.columns = ['show_id','title','director','cast','country','date_added','release_year','rating,duration','listed_in','description','type']
+file['description'] = file['description'].str.lower().str.split()  
+
+stop = stopwords.words('english')
+
+file['Title'] = file['Title'].apply(lambda x: [item for item in x if item not in stop])
+file['Body'] = file['Body'].apply(lambda x: [item for item in x if item not in stop])
+
+file.to_csv("netflix_titles_nov_2019_removed_stop.csv")
