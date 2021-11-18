5d247bd0208e3d8e0c2de52b42040bb99d0b2633
paulcourt <77756501+paulcourt@users.noreply.github.com>
2021-11-18 14:55:33 -0600

    Refactored Columns are now two.

--- remove_columns.py
+++ remove_columns.py
@@ -5,51 +5,20 @@
 import pandas as pd
 from IPython.display import display
 
-## Internal path for accessing data in netflix_titles_nov_2019
+## Internal path for accessing data in netflix_titles_nov_2020
 # files downloaded and stored in SE 512 folder of my computer.
 
-# The following block only executes if the script is run directly
-if __name__ == "__main__":
-    ShowInfo = pd.read_csv(r"netflix_titles.csv")
+##################################################################################
+# Function to decrease the columns in the CSV file to just Title and Description.
+##################################################################################
 
-    #Number of data rows total.
-    n = ShowInfo.shape[0]
-
-    #Number of columns of data.
-    d = ShowInfo.shape[1]
-
-    print ('Rows = ', n, 'Columns = ', d)
-
-    #Displays the first few rows of the data frame.
-    display(ShowInfo.head())
-
-    #Establishes a data frame to modify.
-    PrimaryShowInfo = pd.DataFrame(ShowInfo)
-
-    #Discards data columns that will not be used.
-    PrimaryShowInfo = PrimaryShowInfo.drop(['show_id', 'director', 'cast', 'country', 'date_added', 'release_year', 'rating', 'duration', 'listed_in', 'type'], axis = 1)
-
-    #Number of data rows total.
-    n = PrimaryShowInfo.shape[0]
-
-    #number of columns of data in the modified set.
-    d = PrimaryShowInfo.shape[1]
-
-    print ('Rows = ', n, 'Columns = ', d)
-
-    #Displays the first few rows of the modified data frame.
-    display(PrimaryShowInfo.head())
-
-##############################################################################
-# Use the original code (above) to implement the function and test case (below)
-##############################################################################
-
-# Input: pandas dataframe
-# Output: pandas dataframe (columns removed)
 def trim_columns(orig_df: pd.DataFrame) -> pd.DataFrame:
 
-    result_df = orig_df
-    print("Not Implemented")
+    #Establishes a data frame to modify.
+    result_df = pd.DataFrame(orig_df)
+
+    #Discards data columns that will not be used.
+    result_df = orig_df.drop(['show_id', 'director', 'cast', 'country', 'date_added', 'release_year', 'rating', 'duration', 'listed_in', 'type'], axis = 1)
 
     return result_df
 
